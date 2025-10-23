3551d33bf33c7a97e3e8bfd214e039004ebc0ed9
Rodion Iamshanov <roiamshanov@gmail.com>
2025-10-23 14:29:06 +0200

    minor changes in working_with_data_file.py

--- pandas/working_with_data_file.py
+++ pandas/working_with_data_file.py
@@ -1,12 +1,8 @@
-# %%
+
 import pandas as pd
 
-# %%
 # using encoding because file has cyrillic characters
-df = pd.read_csv('data.txt', encoding='windows-1251')
+df = pd.read_csv('pandas/data.txt', encoding='windows-1251')
 
 
-# %%
-df.head()
-
-# %%
+print(df)
