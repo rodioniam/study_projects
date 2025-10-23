c07dd003c1319d10a7e39cc08c11da9ab67d2e1d
Rodion Iamshanov <roiamshanov@gmail.com>
2025-10-23 18:32:39 +0200

    updated files in pandas folder

--- pandas/working_with_data_file.py
+++ pandas/working_with_data_file.py
@@ -1,8 +1,8 @@
-
+# %%
 import pandas as pd
-
+# %%
 # using encoding because file has cyrillic characters
-df = pd.read_csv('pandas/data.txt', encoding='windows-1251')
+df = pd.read_csv('data.txt', encoding='windows-1251')
 
-
+# %%
 print(df)
