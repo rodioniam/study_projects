6aca5e25412ce5922bc4bb3d598f5baa6c994046
Rodion Iamshanov <roiamshanov@gmail.com>
2025-12-22 17:30:42 +0100

    file updated

--- advanced/class.py
+++ advanced/class.py
@@ -13,6 +13,7 @@ class Car:  # classes should be NamedLikeThis
         print('Car stopped')
 
 
+# 'self' id basically a variable, this class will be assigned to, telling methods to operate on itself
 my_car = Car()
 # it is the same as
 my_list = list((1, 2, 3))  # [1, 2, 3]
@@ -34,3 +35,50 @@ number_2 = Pow()
 
 number_1.pow_num()
 number_2.pow_num()
+
+# this is how i can make my own attributes
+
+
+class Comment:
+    # this will be mandatory argument, each new object from this class will have
+    def __init__(self, text):
+        self.text = text
+        self.votes_qty = 0
+
+    # this way i can define logic for attribute
+    def upvote(self):
+        self.votes_qty += 1
+
+
+first_comment = Comment('Text for a comment')
+
+print(first_comment.text)
+print(first_comment.votes_qty)
+
+first_comment.upvote()
+
+print(first_comment.text)
+print(first_comment.votes_qty)
+
+print(first_comment.__dict__)  # {'text': 'Text for a comment', 'votes_qty': 1}
+
+# a little upgrade
+
+
+class Comment_2:
+    def __init__(self, text):
+        self.text = text
+        self.votes_qty = 0
+
+    # i can change it so i can give a custom number and give it default value
+    def upvote(self, qty=1):
+        self.votes_qty += qty
+
+
+second_comment = Comment_2('Text for a second comment')
+
+print(second_comment.votes_qty)
+
+second_comment.upvote(10)
+
+print(second_comment.votes_qty)
