091f3a0e299a0ba4c1ced86625d3afb439ef34f8
Rodion Iamshanov <roiamshanov@gmail.com>
2025-12-22 16:12:33 +0100

    file updated

--- advanced/class.py
+++ advanced/class.py
@@ -1,5 +1,8 @@
+import numpy as np
+
 # class is some kind of template for creating objects
 # like list class or dict, float, etc
+
 
 class Car:  # classes should be NamedLikeThis
     # this is class methods, never forget (self)!
@@ -18,3 +21,16 @@ my_car.move_car()
 my_car.stop_car()
 # just like
 my_list.append(5)  # [1, 2, 3, 5]
+
+
+class Pow:
+    def pow_num(self):
+        num = np.random.randint(low=1, high=24)
+        print(num**2)
+
+
+number_1 = Pow()
+number_2 = Pow()
+
+number_1.pow_num()
+number_2.pow_num()
