24749a0d15ee96323f7052d1418bc0f0cdf64911
Rodion Iamshanov <roiamshanov@gmail.com>
2025-12-23 15:48:54 +0100

    file updated

--- advanced/class.py
+++ advanced/class.py
@@ -7,10 +7,10 @@ import numpy as np
 class Car:  # classes should be NamedLikeThis
     # this is class methods, never forget (self)!
     def move_car(self):
-        print('Car is moving')
+        return 'Car is moving'
 
     def stop_car(self):
-        print('Car stopped')
+        return 'Car stopped'
 
 
 # 'self' id basically a variable, this class will be assigned to, telling methods to operate on itself
@@ -18,23 +18,25 @@ my_car = Car()
 # it is the same as
 my_list = list((1, 2, 3))  # [1, 2, 3]
 
-my_car.move_car()
-my_car.stop_car()
+my_car.move_car()  # Car is moving
+my_car.stop_car()  # Car stopped
 # just like
 my_list.append(5)  # [1, 2, 3, 5]
+# or
+Car.move_car(my_car)  # Car is moving
 
 
 class Pow:
     def pow_num(self):
         num = np.random.randint(low=1, high=24)
-        print(num**2)
+        return num**2
 
 
 number_1 = Pow()
 number_2 = Pow()
 
-number_1.pow_num()
-number_2.pow_num()
+number_1.pow_num()  # gives different value everytime
+number_2.pow_num()  # gives different value everytime
 
 # this is how i can make my own attributes
 
@@ -52,15 +54,15 @@ class Comment:
 
 first_comment = Comment('Text for a comment')
 
-print(first_comment.text)
-print(first_comment.votes_qty)
+first_comment.text  # Text for a comment
+first_comment.votes_qty  # 0
 
 first_comment.upvote()
 
-print(first_comment.text)
-print(first_comment.votes_qty)
+first_comment.text  # Text for a comment
+first_comment.votes_qty  # 1
 
-print(first_comment.__dict__)  # {'text': 'Text for a comment', 'votes_qty': 1}
+first_comment.__dict__  # {'text': 'Text for a comment', 'votes_qty': 1}
 
 # a little upgrade
 
@@ -77,8 +79,58 @@ class Comment_2:
 
 second_comment = Comment_2('Text for a second comment')
 
-print(second_comment.votes_qty)
+second_comment.votes_qty  # 0
 
 second_comment.upvote(10)
 
-print(second_comment.votes_qty)
+second_comment.votes_qty  # 10
+
+
+# static methods
+# a method that belongs to a class but does not access or modify the class's state or a specific instance's state
+# It behaves like a regular function that is logically grouped within the class's namespace for organizational purposes
+
+
+class Comment_3:
+    def __init__(self, text):
+        self.text = text
+
+    @staticmethod  # this is decorator
+    def merge_comments(first, second):
+        return f"{first} {second}"
+
+
+third_comment = Comment_3('Text for the third comment')
+
+m1 = Comment_3.merge_comments('Hello', 'World!')  # Hello World!
+
+# because 'merge_comments' method does not have 'self' it will not have access to the variable parameters
+m2 = third_comment.merge_comments('Hello', 'Again!')  # Hello Again!
+third_comment.__dict__  # {'text': 'Text for the third comment'}
+
+# class's own attributes
+# they are defind in the body of the class and shared between all objects
+
+
+class Messages:
+    total_messages = 0
+
+    def __init__(self, text):
+        self.text = text
+        self.msg_rating = 0
+        Messages.total_messages += 1
+
+
+msg1 = Messages('First message')
+
+Messages.total_messages  # 1
+
+msg2 = Messages('Second message')
+
+Messages.total_messages  # 2
+# i can rewrite this attribute if i call it directly
+Messages.total_messages = 200
+Messages.total_messages  # 200
+
+msg3 = Messages('Third message')
+Messages.total_messages  # 201
