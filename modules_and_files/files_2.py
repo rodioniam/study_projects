68c930a40cca6d3369c7f2747bce7b190990e0aa
Rodion Iamshanov <roiamshanov@gmail.com>
2026-01-08 18:24:40 +0100

    added new files, updated some files

--- modules_and_files/files_2.py
+++ modules_and_files/files_2.py
@@ -60,3 +60,42 @@ if make_dir.exists():
     print('Directory deleted!')
 else:
     print('Directory does not exist.')
+
+
+# open and reading files
+with open('modules_and_files/test_file.txt') as test_file:
+    # reads as string
+    read_file = test_file.read()
+
+print(read_file)
+
+with open('modules_and_files/test_file.txt') as test_file:
+    # reads as each row as element in list
+    read_lines = test_file.readlines()
+
+print(read_lines)
+
+# to write in a file i need to do this
+# it will overwrite everthing in the file!
+
+with open('modules_and_files/test_file.txt', 'w') as write_file:
+    write_file.write('Added new line\n')
+
+# if i dont want to overwrite file i can do this
+
+with open('modules_and_files/test_file.txt', 'a') as file_to_add:
+    file_to_add.write('Added one more line to the file\n')
+
+with open('modules_and_files/test_file.txt') as read:
+    print(read.read())
+
+
+# to delete file i can use this
+try:
+    Path('modules_and_files/test_file_delete.txt').unlink()
+    print('File deleted!')
+except Exception as e:
+    print(e)
+
+# here is how i can create new file
+new_file = open('modules_and_files/creating_file.txt', 'w')
